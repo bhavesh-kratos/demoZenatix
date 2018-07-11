52c7fe927d67017355f99a78e005720c06976b05
bhavesh-kratos <bp3955@gmail.com>
2018-07-11 21:23:28 +0530

    completed + ui fixes - uploading not fixed

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => (
   <div className="App">
     <Switch>
       <PublicRoute exact path ="/login" component={LoginScreen} />  
-      <Route exact path ="/" component={ExamScreen} />  
+      <PrivateRoute exact path ="/" component={ExamScreen} />  
       <PrivateRoute exact path ="/score" component={ShareScreen} />  
     </Switch>
   </div>
