6b6c0f015d813f657b5480cb42baf9d3c55237d0
bhavesh-kratos <38069284+bhavesh-kratos@users.noreply.github.com>
2018-07-20 21:48:04 +0530

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 export const DROPBOX_KEY = '';
 export const EXAM_TIME = '180'; //In seconds
 export const AUTO_LOGIN = true;
-export const FB_APPID = '';  // firebase: 208016080036434, local: 224905925001825
+export const FB_APPID = '';  
 export const IMGUR_ID = '';
